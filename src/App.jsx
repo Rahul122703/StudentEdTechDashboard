677246c6fe0562274e7042e561bf5a9db71ff5f8
rahul122703 <killbusyness@gmail.com>
2025-08-02 13:04:09 +0530

    adding new project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='border border-black h-screen w-screen grid place-items-center'>
+      <div className="border border-black">RAHUL SHARMA</div>
+    </div>
+  )
+}
+
+export default App
